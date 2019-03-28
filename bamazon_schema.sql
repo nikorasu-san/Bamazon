63b293af8cf3b442a695412c5c5148b707dadeeb
Liz Lemon <nikorasu.dx@gmail.com>
2019-03-28 17:45:54 -0500

    initial push

---
+++ bamazon_schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products(
+item_id INTEGER(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+product_name VARCHAR(35) NOT NULL,
+department_name VARCHAR(20),
+price DECIMAL(10,2) NOT NULL,
+stock_quantity INTEGER(10) DEFAULT 0
+);
