6d7c7b4a68513b9268487b5236fb7e45be905673
Liz Lemon <nikorasu.dx@gmail.com>
2019-03-30 15:46:12 -0500

    add stock fin

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,19 +17,21 @@ function homeManager() {
     inquirer.prompt([
         {
             type: "list",
-            message: "Hello, Manager. What would you like to do?",
-            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"],
+            message: "What would you like to do?",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"],
             name: "command"
         }
     ]).then(function (answers) {
         switch (answers.command) {
             case "View Products for Sale": viewProducts();
                 break;
-            case "View Low Inventory": console.log("b"); backToStart();
+            case "View Low Inventory": viewLow();
                 break;
-            case "Add to Inventory": console.log("c"); backToStart();
+            case "Add to Inventory": addStock();
                 break;
             case "Add New Product": console.log("d"); backToStart();
+                break;
+            case "Exit": connection.end();
                 break;
             default: console.log("how did you get here")
         }
@@ -39,12 +41,47 @@ function homeManager() {
 // start call stack
 homeManager()
 
+// define function to see all products in stock
 function viewProducts() {
     connection.query('SELECT item_id, product_name, price, stock_quantity from products WHERE stock_quantity > 0', function (error, results, fields) {
         if (error) throw error;
         console.table("Products in stock:", results)
-        backToStart();
+        homeManager();
     });
+}
+
+// define function to view inventory lower than 5
+function viewLow() {
+    connection.query('SELECT item_id, product_name, stock_quantity from products WHERE stock_quantity < 5', function (error, results, fields) {
+        if (error) throw error;
+        console.table("Products with less than 5 in stock:", results)
+        homeManager();
+    });
+}
+
+// add inventory for an item
+function addStock() {
+    inquirer.prompt([
+        {
+            message: "Which item id do you want to restock?",
+            name: "item_id"
+        },
+        {
+            message: "How many units are you adding to the stock?",
+            name: "add"
+        }
+    ]).then(function (answers) {
+        let stock = 0;
+        connection.query(`SELECT stock_quantity FROM products WHERE item_id=${answers.item_id}`, function (error, firstResult) {
+            stock = parseInt(firstResult[0].stock_quantity);
+            newStock = stock + parseInt(answers.add)
+            connection.query(`UPDATE products SET stock_quantity = ${newStock} WHERE item_id = ${answers.item_id}`, function (error, results, fields) {
+                if (error) throw error;
+                console.table("stock updated")
+                homeManager();
+            })
+        })
+    })
 }
 
 // Ask the user if they want to run the app again or quit
